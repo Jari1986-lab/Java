fd20941158dc9dd7ab801ca3c402daa39921c923
jari <iceman198611@outlook.com>
2022-02-11 05:53:04 +0200

    udpate trainee

--- Jari/src/main/java/com/example/Jari/Employee.java
+++ Jari/src/main/java/com/example/Jari/Employee.java
@@ -1,6 +1,6 @@
-/*package com.example.Jari;
+package com.example.Jari;
 
-import org.springframework.web.bind.annotation.SessionAttributes;
+//import org.springframework.web.bind.annotation.SessionAttributes;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +10,7 @@ import lombok.ToString;
 @Getter
 @ToString
 
-public class Employee {
+    public class Employee {
 
     private String name;
     
@@ -22,6 +22,45 @@ public class Employee {
 
     private String department;
 
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Long getAge() {
+        return age;
+    }
+
+    public void setAge(Long age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
 }
-*/
